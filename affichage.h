d3297247301f13972e5e6eaa5e437657a1c2738c
Ant-HonyB <73540798+Ant-HonyB@users.noreply.github.com>
2021-01-02 13:24:11 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -2,13 +2,13 @@
 #define AFFICHAGE
 
 
-//------------------------------------//:INCLUDE & DEFINE:\\-------------------------------------\\
+//------------------------------------//:INCLUDE & DEFINE:\\-------------------------------------|
 
 #include "utils.h"
 #define clrscr() printf("\033[H\033[2J") // la fonction clrscr(); effacera l'écran
 #define couleur(param) printf("\033[%sm",param) //paramétrage de l'affichage en couleur
 
-//-------------------------------//:PROTOTYPES DES FONCTIONS:\\----------------------------------\\
+//-------------------------------//:PROTOTYPES DES FONCTIONS:\\----------------------------------|
 
 void afficher(int nrow, int ncol, Case matrice[nrow][ncol]);
 
