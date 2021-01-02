c1b581e027fa879d83f02545cf9056c364845e85
Ant-HonyB <73540798+Ant-HonyB@users.noreply.github.com>
2021-01-02 13:27:27 +0100

    Update initialisation.h

--- initialisation.h
+++ initialisation.h
@@ -1,14 +1,14 @@
 #ifndef INITIALISATION
 #define INITIALISATION
 
-//------------------------------------//:INCLUDE & DEFINE:\\-------------------------------------\\
+//------------------------------------||:INCLUDE & DEFINE:||-------------------------------------||
 
 #include "utils.h"
 #define PROB_V 5
 #define PROB_L 20
 #define PROB_S 10
 
-//-------------------------------//:PROTOTYPES DES FONCTIONS:\\----------------------------------\\
+//-------------------------------||:PROTOTYPES DES FONCTIONS:||----------------------------------||
 
 void initialisation(Bonhomme **soignant, Bonhomme **lambda, int * cpt_lambda, int * cpt_virus, int * cpt_soignant, int nrow, int ncol, Case emplacement[nrow][ncol], int vie_virus[]);
 int pioche(int min, int max);
